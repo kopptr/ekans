1de0f9f0c0d046544a2698f48228f80df722627c
Samuel C. Payson <scpayson@gmail.com>
2011-01-01 15:38:12 -0500

    Added simple template/test code for starting the agents

--- agent.c
+++ agent.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include "cfg.h"
 #include "rwq.h"
 
 static pthread_t * agents;
 
-void start_agents(void) {
-   
+static void * agent_main(void * v_args) {
+   fprintf(stderr, "In thread %d\n", (int)v_args);
+   pthread_exit(0);
 }
+/* This function is allowed to kill the program, and will print a report      *
+ * to stderr.                                                                 */
+void start_agents(void) {
+   int i;
+   agents = malloc(sys_cfg.n_threads * sizeof(pthread_t));
+   for (i = 0; i < sys_cfg.n_threads; ++i) {
+      pthread_create(agents + i, NULL, agent_main, (void *)i);
+   }
+   fprintf(stderr, "%s done.\n", __FUNCTION__);
+}
+
---
+++ include/agent.h
@@ -0,0 +1,6 @@
+#ifndef _EKANS_AGENT_H_
+#define _EKANS_AGENT_H_
+
+void start_agents(void);
+
+#endif /* agent.h */
