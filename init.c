aa49e7cff0fc5b3db16cf7797b8c46d067b6c123
Samuel C. Payson <scpayson@gmail.com>
2011-01-02 15:00:05 -0500

    Added return statements to init stubs, to suppress warnings.

--- init.c
+++ init.c
@@ -3,9 +3,9 @@
 /* Phase 1 is for before the agents are active and is responsible for  *
  * initializing data such as the client-queue.                         */
 int init_phase_1(void) {
-   
+   return 0;
 }
 
 int init_phase_2(void) {
-   
+   return 0;
 }
