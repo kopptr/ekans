856bf58911cbcdfbea5596ba5df59d4d6cdd5530
Samuel C. Payson <scpayson@gmail.com>
2011-01-02 14:49:46 -0500

    Dispatch now does a basic subset of its final job.

--- dispatch.c
+++ dispatch.c
@@ -1,8 +1,16 @@
 #include "init.h"
+#include "agent.h"
+#include "rwq.h"
+#include "tcp.h"
 
 void run_dispatch(void) {
+   SOCK listener, client;
    init_phase_1();
-   /* start_agents(); */
+   start_agents();
    init_phase_2();
-   /* dispatch main loop goes here */
+   listener = get_listener(sys_cfg.port);
+   for (;;) {
+      client = accept(listener, NULL, NULL);
+      rwq_add(client);
+   }
 }
