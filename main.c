72f1115031ea1f1bd2fa4b9ff575f71fbbf9b3e8
Samuel C. Payson <scpayson@gmail.com>
2011-01-02 15:05:17 -0500

    Rudimentary version of main() complete.

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
 #include "http.h"
+#include "dispatch.h"
 #include "rwq.h"
-
-void p_err(const char * str) {
-   fprintf(stderr, "%s: errno %d: %s\n", str, errno, strerror(errno));
-}
+#include "cfg.h"
 
 int main(int argc, char * argv[]) {
+   parse_args(argc, argv);
+   printf("Ekans webserver starting up on port %s\n", sys_cfg.port);
    rwq_init();
    http_init_regex();
-   
+   run_dispatch();
+   return 0;
 }
